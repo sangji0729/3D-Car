b285c19859806066f09249f6f6b0daaa22d04680
sangji0729 <sangji0729@naver.com>
2021-08-30 20:47:37 +0900

    운전시스템 구현
    운전시스템 구현

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -8,19 +8,60 @@ public class PlayerMove : MonoBehaviour
     public float moveSpeed;
     public float horizontalInput;
     public float turnSpeed;
-   
+    public Rigidbody rigid;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool forward = Input.GetButton("forward");
+        bool backward = Input.GetButton("backward");
+        bool right = Input.GetButton("right");
+        bool left = Input.GetButton("left");
+        
         horizontalInput = Input.GetAxis("Horizontal");
         move = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * move);
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        
+        if(forward == true)
+        {
+            //transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+            rigid.AddForce(transform.forward * moveSpeed, ForceMode.Impulse);
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                rigid.AddForce(transform.forward * (moveSpeed * 1.5f), ForceMode.Impulse);
+            }
+            if(right == true)
+            {
+                transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+                //rigid.AddForce(300, 0, 0, ForceMode.Impulse);
+            }
+            if(left == true)
+            {
+                transform.Rotate(Vector3.down, turnSpeed * Time.deltaTime);
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rigid.AddForce(new Vector3(0, 0, 0), ForceMode.Impulse);
+            }
+        }
+        if(backward == true)
+        {
+            //transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+            rigid.AddForce(-transform.forward * moveSpeed, ForceMode.Impulse);
+            //if car goes back, right and left key will reverse
+            if (right == true)
+            {
+                transform.Rotate(Vector3.down, turnSpeed * Time.deltaTime);
+            }
+            if (left == true)
+            {
+                transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+            }
+        }
+       
     }
 }
